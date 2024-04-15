2b3afba542c1b625d5979599f40c1b9af15be585
VinciGit00 <mvincig11@gmail.com>
2024-04-15 15:50:18 +0200

    add new csv

---
+++ Dataset/Dataset_fixing.R
@@ -0,0 +1,15 @@
+setwd("~/Github/Statistical-learning-project/Dataset")
+
+df <- read.csv("Sleep_health_and_lifestyle_dataset.csv")
+
+
+correlation<- cor(model.matrix(~.-1,data=df));
+## High correlation: Stess level (-0.81)
+
+correlation_high <- correlation[correlation[, "Sleep.Duration"] > 0.8 | correlation[, "Sleep.Duration"] < -0.8, ];
+correlation_high <- correlation_high[ "Sleep.Duration"];
+
+#High correlation: Stress.Level and Quality.of.Sleep, we decide just to remove Quality.of.Sleep
+df <- subset(df, select = -c(Quality.of.Sleep, Person.ID));
+
+write.csv(df, "Sleep_health_and_lifestyle_dataset_adjusted.csv")
