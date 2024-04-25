1831e75eaac7dd88afb44e5f2173bbcfce3871a4
VinciGit00 <mvincig11@gmail.com>
2024-04-25 18:08:09 +0200

    add random forest and regression tree

--- 11_Random_forest.R
+++ 11_Random_forest.R
@@ -6,8 +6,8 @@ set.seed(22)
 
 setwd("~/Github/Statistical-learning-project/Dataset")
 
-df <- read.csv("Sleep_health_and_lifestyle_dataset_adjusted.csv")
-
+df <- read.csv("Sleep_health_and_lifestyle_dataset_adjusted_gam.csv")
+df$Blood.Pressure<-as.character(df$Blood.Pressure) # rendo i valori stringhe
 # Types of the columns of the dataset:
 # Gender: character
 # Age:integer
@@ -38,9 +38,74 @@ df <- df[, !(names(df) %in% c("Gender", "Occupation", "BMI.Category", "Blood.Pre
 # BMI.CategoryObese
 # Sleep.DisorderInsomnia
 
-df <- subset(df, select = -c(GenderMale, OccupationDoctor, `Blood.Pressure115/78`, BMI.CategoryObese, Sleep.DisorderInsomnia))
+df <- subset(df, select = -c(GenderMale, OccupationDoctor, `Blood.Pressure11578`, BMI.CategoryObese, Sleep.DisorderInsomnia))
 
 df <- df %>% mutate_all(~(scale(.) %>% as.vector))
 train_lines <- sample(dim(df)[1], round(dim(df)[1]*0.7))
 
-df <- df[train_lines, ]
+names(df)[names(df) == 'OccupationSales Representative'] <- 'OccupationSalesRepresentative'
+names(df)[names(df) == 'OccupationSoftware Engineer'] <- 'OccupationSoftwareEngineer'
+names(df)[names(df) == 'BMI.CategoryNormal Weight'] <- 'BMI.CategoryNormalWeight'
+names(df)[names(df) == 'Sleep.DisorderSleep Apnea'] <- 'Sleep.DisorderSleepApnea'
+
+
+# Divisione dei dati in set di addestramento e test
+train_lines <- sample(dim(df)[1], round(dim(df)[1]*0.7))
+train_df <- df[train_lines, ]
+test_df <- df[-train_lines, ]
+
+library ( randomForest )
+
+#mtry = Number of variables randomly sampled as candidates at each split.
+#ntree = Number of trees to grow.
+bagg_model <- randomForest(Sleep.Duration ~ . ,data= train_df)
+
+bagg_model
+plot(bagg_model)
+
+yhat <- predict(bagg_model, newdata = df[-train,])
+plot(yhat,df$Sleep.Duration[-train])
+abline(0,1)
+
+mse <- mean((yhat - df$medv[-train])^2)
+
+# how to change number of tree? 
+# add ntree oprion 
+bagg_model <- randomForest(Sleep.Duration ~ . ,data = train_df,
+                           mtry = ncol(df)-1, importance = TRUE, ntree = 100)
+bagg_model
+
+importance(bagg_model)
+
+# Random Forest
+# default p = sqrt(m) -> see doc 
+forest_model <- randomForest(Sleep.Duration ~ . ,data = train_df,
+                             mtry = floor(sqrt(ncol(df)-1)), importance = TRUE, ntree = 100)
+forest_model
+
+# if the object has a non-null test component, then the returned object is a 
+# matrix where the first column is the out-of-bag estimate of error, 
+# and the second column is for the test set.
+plot(forest_model,type = 'b',col="green",pch = "+")
+par(new=TRUE)
+plot(bagg_model,type = 'b',col="red",pch='o')
+
+yhat <- predict(forest_model, newdata = test_df)
+res <- yhat - test_df$Sleep.Duration
+mean_res <- mean(res)
+mse <- mean((res)^2) 
+importance(forest_model)
+
+shapiro_test <- shapiro.test(res)
+print("Shapiro-Wilk test per la normalità dei residui:")
+print(shapiro_test)
+
+# Disegna l'istogramma dei residui
+hist(res, main = "Istogramma dei Residui", xlab = "Residui")
+
+# Stampa un messaggio se i residui non sono normalmente distribuiti
+if (shapiro_test$p.value < 0.05) {
+  cat("I residui non seguono una distribuzione normale (p-value < 0.05).\n")
+} else {
+  cat("I residui seguono una distribuzione normale (p-value >= 0.05).\n")
+}
--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 # Statistical Learning Course Repository
 
 Official repository for the Statistical Learning course for the Master's degree at the University of Bergamo.
-All the algorithms are implemented in R
+All the algorithms are implemented in R programming language
 
 ![key1](Assets/logo.jpeg)
 
@@ -10,13 +10,13 @@ All the algorithms are implemented in R
 - [x] 1 - Leave-one-out validation with polynomial/linear regression
 - [x] 2 - Cross-validation (kfold) with polynomial/linear regression
 - [ ] 3 - Bootstrap with polynomial/linear regression
-- [ ] 4 - Forward stepwise with polynomial/linear regression
-- [ ] 5 - Backward stepwise with polynomial/linear regression
+- [x] 4 - Forward stepwise with linear regression
+- [x] 5 - Backward stepwise with linear regression
 - [x] 6 - Lasso regression with CV for choosing the best lambda
 - [x] 7 - Ridge regression with CV for choosing the best lambda
 - [x] 8 - GAM with smoothing splines
 - [x] 9 - GAM with local regression
-- [ ] 10 - Regression tree 
-- [ ] 11 - Random forest
+- [x] 10 - Regression tree 
+- [x] 11 - Random forest
 - [ ] 12 - Boosting (optional)
 - [ ] 13 - Bagging (optional)
